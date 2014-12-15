908d55d8dfa5d20405213eda4c9c4c15d0a076a8
SingleX <singlexer@gmail.com>
2014-12-15 17:51:05 +0800

    init

---
+++ src/main/java/net/singlex/app/MainFunc.java
@@ -0,0 +1,10 @@
+package net.singlex.app;
+
+public class MainFunc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("MainFunc...");
+	}
+
+}
---
+++ src/test/java/net/singlex/app/TestMain.java
@@ -0,0 +1,5 @@
+package net.singlex.app;
+
+public class TestMain {
+	
+}
